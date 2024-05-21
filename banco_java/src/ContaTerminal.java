00426c3a5696169c3eb2a0768e33505002dd385c
hideki_1 <uedahideki10@gmail.com>
2024-05-21 16:23:59 -0300

    banco

--- banco_java/src/ContaTerminal.java
+++ banco_java/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner; // Importe a classe Scanner para poder usar para ler a entrada do usuário
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // Crie um objeto Scanner para ler a entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número do usuário:");
+        int usuario = scanner.nextInt();
+
+        System.out.println("Digite o número da agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o nome do usuário:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o saldo do usuário:");
+        double saldo = scanner.nextDouble();
+
+        // Imprima os valores lidos com uma mensagem mais clara e bem formatada
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + usuario + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
+
+        // Não se esqueça de fechar o scanner depois de usá-lo
+        scanner.close();
     }
 }
